41f96c5b7edab0691a242961f053c31163d33b01
Krzysztof Dziurawiec <chris84kd@googlemail.com>
2016-06-25 00:17:52 +0100

    Created Vidly repo

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace Vidly
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "MoviesByRealeaseDate",
+                "movies/released/{year}/{month}",
+                new {controller="Movies", action="ByRealeaseDate"}
+                );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id: " + id);
+        }
+
+        //movies
+        public ActionResult Index(int? indexPage, string sortBy)
+        {
+            if (!indexPage.HasValue)
+            {
+                indexPage = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("index page:{0} sort by:{1}", indexPage, sortBy));
+        }
+        public ActionResult ByRealeaseDate(int year, int month)
+        {
+            return Content("date:"+year+"/"+month);
+        }
+    }
+}
