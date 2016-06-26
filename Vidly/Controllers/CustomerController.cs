bae23a06a4718df8332f146f35eed87097a76fbe
Krzysztof Dziurawiec <chris84kd@googlemail.com>
2016-06-26 23:57:51 +0100

    Added Edit and New for Customer

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -20,6 +20,51 @@ namespace Vidly.Controllers
         {
             _context.Dispose();
         }
+
+        //adding new customer
+        public ActionResult New()
+        {
+            var membershipType = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipType
+            };
+            return View("CustomerForm",viewModel);
+        }
+
+        //editing customer
+        public ActionResult Edit(int Id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == Id);
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        //saving customer
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id==0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDB = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDB.Name = customer.Name;
+                customerInDB.Birthday = customer.Birthday;
+                customerInDB.MembershipTypeId = customer.MembershipTypeId;
+                customerInDB.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index","Customer");
+        }
+
         // GET: Customer
         public ActionResult Index()
         {
@@ -27,6 +72,7 @@ namespace Vidly.Controllers
             return View(customers);
         }
 
+        // display customer details
         public ActionResult Details(int Id)
         { 
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(x => x.Id == Id);
@@ -35,6 +81,5 @@ namespace Vidly.Controllers
             else
                 return HttpNotFound(); 
         }
-
     }
 }
