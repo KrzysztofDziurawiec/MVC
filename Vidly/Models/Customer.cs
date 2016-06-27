371bf3505eaa8f28af309f874a25ac2ba802f1ef
Krzysztof Dziurawiec <chris84kd@googlemail.com>
2016-06-27 23:05:10 +0100

    Done up to validation (53)

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -21,6 +21,7 @@ namespace Vidly.Models
         [Display(Name="Membership Type")]
         public byte MembershipTypeId { get; set; }
 
+        [Min18YearsIfAMember]
         [Display(Name="Date of birth")]
         public DateTime? Birthday { get; set; }
     }
---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+            if (customer.MembershipTypeId==MembershipType.Unknown ||
+                customer.MembershipTypeId == MembershipType.PayAsYouGo)
+                return ValidationResult.Success;
+            if (customer.Birthday == null)
+                return new ValidationResult("Birthdate requiered");
+            var age = DateTime.Now.Year - customer.Birthday.Value.Year;
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Need to be over 18 for membership");
+        }
+    }
+}
